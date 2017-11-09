f15d1d7f1c8c02419b660bb7cccd253add8eb04c
eshaft <eshaft@gmail.com>
2017-11-09 14:34:46 +0300

    routes

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const express = require('express'),
       process = require('process'),
-      socketio = require('socket.io');
+      socketio = require('socket.io'),
+      router = require('./routes.js'),
+      sockets = require('./sockets.js');
 
 var app = express();
 var server = app.listen(process.argv[2]);
@@ -8,9 +10,15 @@ var io = socketio(server);
 
 app.use(express.static('static'));
 
+//setup express to use middleware
+app.use('/', router);
+
+var bears = io.of('/bears');
+var cubs = io.of('/cubs');
+bears.on('connection', sockets.bearsNamespace);
+cubs.on('connection', sockets.cubsNamespace);
+
 io.on('connection', (socket) => {
-  socket.on('socketping', () => {
-    console.log('Received socketping, sending socketpong');
-    socket.emit('socketpong');
-  });
+    //we need specific events for each
+    //cubs and bears
 });
