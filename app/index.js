a21c9fa7fcdea523740fd703d30802ca43913a45
eshaft <eshaft@gmail.com>
2017-11-09 14:06:40 +0300

    sessions

--- app/index.js
+++ app/index.js
@@ -1,16 +1,41 @@
 const express = require('express'),
       process = require('process'),
-      socketio = require('socket.io');
+      socketio = require('socket.io'),
+      session = require('express-session');
 
 var app = express();
 var server = app.listen(process.argv[2]);
 var io = socketio(server);
 
+var sessionMiddleware = session({
+    secret: 'make this a good secret',
+    resave: false,
+    saveUninitialized: true
+});
+
+//setup express to use middleware
+app.use(sessionMiddleware);
+app.use((req, res, next) => {
+    console.log(`From Express: ${req.session.name}`);
+    next();
+});
+
 app.use(express.static('static'));
 
+//socket.io middleware
+io.use((socket, next) => {sessionMiddleware(socket.request, {}, next);});
+
 io.on('connection', (socket) => {
-  socket.on('socketping', () => {
-    console.log('Received socketping, sending socketpong');
-    socket.emit('socketpong');
-  });
+    console.log(socket.request.session);
+    if(socket.request.session.name !== undefined){
+        socket.emit('name', socket.request.session.name);
+        io.emit('event', socket.request.session.name + ' has joined!');
+    }
+
+
+    socket.on('name', (name) => {
+        socket.request.session.name = name;
+        socket.request.session.save();
+        socket.broadcast.emit('event', name + ' says hello!');
+    });
 });
