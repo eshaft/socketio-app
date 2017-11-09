4e7035f108682f177b0a407ca589a70b3ab29416
eshaft <eshaft@gmail.com>
2017-11-09 14:52:23 +0300

    multiple-server

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const express = require('express'),
       process = require('process'),
-      socketio = require('socket.io');
+      socketio = require('socket.io'),
+      config = require('./config.js'),
+      socketioRedis = require('socket.io-redis');
 
 var app = express();
 var server = app.listen(process.argv[2]);
@@ -8,9 +10,22 @@ var io = socketio(server);
 
 app.use(express.static('static'));
 
+io.adapter(socketioRedis({host: config.redis_host, port: config.redis_port}));
 io.on('connection', (socket) => {
-  socket.on('socketping', () => {
-    console.log('Received socketping, sending socketpong');
-    socket.emit('socketpong');
-  });
+    socket.on('room.join', (room) => {
+        console.log(socket.rooms);
+        Object.keys(socket.rooms).filter((r) => r != socket.id)
+            .forEach((r) => socket.leave(r));
+
+        setTimeout(() => {
+            socket.join(room);
+            socket.emit('event', 'Joined room ' + room);
+            socket.broadcast.to(room).emit('event', 'Someone joined room ' + room);
+        }, 0);
+    })
+
+    socket.on('event', (e) => {
+        socket.broadcast.to(e.room).emit('event', e.name + ' says hello!');
+    });
+
 });
