d9bac1824f9e4f6ec13ff28907942f2ba307cb54
eshaft <eshaft@gmail.com>
2017-11-09 12:43:10 +0300

    rooms-app

--- app/index.js
+++ app/index.js
@@ -9,8 +9,20 @@ var io = socketio(server);
 app.use(express.static('static'));
 
 io.on('connection', (socket) => {
-  socket.on('socketping', () => {
-    console.log('Received socketping, sending socketpong');
-    socket.emit('socketpong');
-  });
+    socket.on('room.join', (room) => {
+        console.log(socket.rooms);
+        Object.keys(socket.rooms).filter((r) => r != socket.id)
+            .forEach((r) => socket.leave(r));
+
+        setTimeout(() => {
+            socket.join(room);
+            socket.emit('event', 'Joined room ' + room);
+            socket.broadcast.to(room).emit('event', 'Someone joined room ' + room);
+        }, 0);
+    })
+
+    socket.on('event', (e) => {
+        socket.broadcast.to(e.room).emit('event', e.name + ' says hello!');
+    });
+
 });
