a518a2ab29cb0e2b88239fac1797d6c769e82aef
eshaft <eshaft@gmail.com>
2017-11-09 12:04:35 +0300

    rooms

--- app/index.js
+++ app/index.js
@@ -9,8 +9,18 @@ var io = socketio(server);
 app.use(express.static('static'));
 
 io.on('connection', (socket) => {
-  socket.on('socketping', () => {
-    console.log('Received socketping, sending socketpong');
-    socket.emit('socketpong');
-  });
+    var now = Date.now();
+    console.log(now);
+    if((now % 2) == 0){
+        socket.join('even');
+    }else{
+        socket.join('odd');
+    }
+
+    io.to('even').emit('event', 'Even Room ' + now);
+    io.to('odd').emit('event', 'Odd Room ' + now);
+    setTimeout(() => {
+        io.to('even').emit('event', 'Even Room');
+        io.to('odd').emit('event', 'Odd Room');
+    }, 5000);
 });
